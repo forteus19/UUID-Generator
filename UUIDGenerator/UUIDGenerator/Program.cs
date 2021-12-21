14333a798dc942aab01356291901105c3e683cea
nakoni9 <66690013+iirdcoycltis@users.noreply.github.com>
2021-12-21 16:57:59 -0500

    Initial commit

---
+++ UUIDGenerator/UUIDGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter the amount of UUIDs you want to generate: ");
+int amount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("--------------------");
+
+if (File.Exists("./output.txt"))
+    File.Delete("./output.txt");
+using StreamWriter file = new("output.txt", append: true);
+
+for (int i = 0; i < amount; i++)
+{
+    Guid guid = Guid.NewGuid();
+    Console.WriteLine(guid.ToString());
+    await file.WriteLineAsync(guid.ToString());
+}
+
+Console.WriteLine("--------------------");
+Console.WriteLine("UUIDs mirrored to output.txt");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
